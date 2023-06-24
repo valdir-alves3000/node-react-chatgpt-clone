7627a35ab59c9faf476a6bfe67f94b0ac56f8e02
valdir-alves3000 <valdiralves3000@gmail.com>
2023-06-23 21:46:02 -0300

    Add OpenAI configuration file

---
+++ server/src/config/openai.js
@@ -0,0 +1,23 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+module.exports = class openai {
+  static configuration() {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    return new OpenAIApi(configuration);
+  }
+
+  static textCompletion({ prompt }) {
+    return {
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    };
+  }
+};
